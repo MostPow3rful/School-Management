978de37daac70e0e3d28efd011d4b99f5983848a
Kian Shahi <96079030+JesusKian@users.noreply.github.com>
2022-12-12 22:29:56 +0330

    Add files via upload
    Debugged When Saving Data in json/Secret.json

--- run.py
+++ run.py
@@ -226,7 +226,7 @@ class Config(Color):
         )
         RESULT = cursor.fetchall()
 
-        os.system('echo "" > log/log.log')
+        os.system('echo " " > log/log.log')
         with open(file="./json/Secret.json", mode="a") as file:
             file.write(
                 '{\n\t"username":"'+RESULT[0][0] +
@@ -269,3 +269,5 @@ def main() -> None:
 if __name__ == "__main__":
     init(autoreset=True)
     main()
+
+
